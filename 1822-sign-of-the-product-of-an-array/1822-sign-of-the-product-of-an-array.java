daf08ceb8cb4fc205940c2b9283e8e212a7aff20
Mahaselvan <mahaselvanoffl@gmail.com>
2025-12-29 22:51:10 +0530

    Time: 0 ms (100%), Space: 45.1 MB (70.87%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -1,5 +1,18 @@
 class Solution {
     public int arraySign(int[] nums) {
-        
+       double product=1;
+        for (int i:nums){
+            product*=i;
+        }
+        if(product<0){
+            return -1;
+        }
+        if(product>0){
+            return 1;
+        }
+        if(product==0){
+            return 0;
+        }
+        return 0;
     }
 }
