9f8875a072253023f190f9f4fcfec4930c18411a
Mahaselvan S <mahaselvanoffl@gmail.com>
2026-03-31 16:46:51 +0530

    Time: 619 ms (69.26%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,13 +1,7 @@
-SELECT 
-    s.user_id,
-    ROUND(
-        IFNULL(
-            SUM(c.action = 'confirmed') / COUNT(c.action),
-            0
-        ),
-        2
-    ) AS confirmation_rate
-FROM Signups s
-LEFT JOIN Confirmations c
-ON s.user_id = c.user_id
-GROUP BY s.user_id;
+select s.user_id,
+
+round(ifnull(sum(c.action="confirmed")/count(c.action),0 ),2) as confirmation_rate
+from Signups s 
+left join Confirmations c 
+on s.user_id = c.user_id 
+group by s.user_id;
