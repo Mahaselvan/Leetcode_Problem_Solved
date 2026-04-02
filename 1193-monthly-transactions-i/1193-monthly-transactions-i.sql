2c772f91460cd2bf37f62a3e6c579cb81317144a
Mahaselvan S <mahaselvanoffl@gmail.com>
2026-04-02 10:02:18 +0530

    Time: 618 ms (28.06%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,11 +1,9 @@
-SELECT 
-    LEFT(trans_date, 7) AS month,
-    country, 
-    COUNT(id) AS trans_count,
-    SUM(state = 'approved') AS approved_count,
-    SUM(amount) AS trans_total_amount,
-    SUM((state = 'approved') * amount) AS approved_total_amount
-FROM 
-    Transactions
-GROUP BY 
-    month, country;
+select
+ DATE_FORMAT(trans_date, '%Y-%m') AS month,
+country,
+count(amount) as trans_count,
+sum(case when state="approved" then 1 else 0 end) as approved_count,
+sum(amount) as trans_total_amount,
+sum(case when state="approved" then amount else 0 end )as approved_total_amount
+from Transactions 
+group by month,country;
