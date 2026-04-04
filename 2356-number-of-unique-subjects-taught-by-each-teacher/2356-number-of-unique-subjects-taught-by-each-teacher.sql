cfd6feac91a9cd8af2f2f24dfdc673c91e6a6331
Mahaselvan S <mahaselvanoffl@gmail.com>
2026-04-04 17:56:23 +0530

    Time: 513 ms (42.51%), Space: 0B (100%) - LeetHub

--- 2356-number-of-unique-subjects-taught-by-each-teacher/2356-number-of-unique-subjects-taught-by-each-teacher.sql
+++ 2356-number-of-unique-subjects-taught-by-each-teacher/2356-number-of-unique-subjects-taught-by-each-teacher.sql
@@ -1,4 +1,5 @@
-SELECT teacher_id,
-COUNT(DISTINCT subject_id) AS cnt
-FROM Teacher
-GROUP BY teacher_id;
+select teacher_id ,
+count( distinct subject_id) as cnt
+
+from Teacher
+group by teacher_id;
